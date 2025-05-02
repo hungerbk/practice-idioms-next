22adf473735387fcb879eb41b2ea585f5e984529
hungerbk <wjantlr@gmail.com>
2025-05-03 02:14:53 +0900

    💄 [Design] shake 애니메이션 생성

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,7 +7,18 @@ const config: Config = {
     './app/**/*.{js,ts,jsx,tsx,mdx}'
   ],
   theme: {
-    extend: {}
+    extend: {
+      keyframes: {
+        shake: {
+          '0%, 100%': { transform: 'translateX(0)' },
+          '25%': { transform: 'translateX(-4px)' },
+          '75%': { transform: 'translateX(4px)' }
+        }
+      },
+      animation: {
+        shake: 'shake 0.2s ease-in-out 0s 2'
+      }
+    }
   },
   plugins: []
 }
